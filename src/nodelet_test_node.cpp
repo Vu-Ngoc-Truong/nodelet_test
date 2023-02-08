2afa9a25e866670f42321eba3c4aa6feb8c66a57
Vu-Ngoc-Truong <truong.vungoc@meiko.vn>
2023-02-08 15:37:52 +0700

    add launch file, add compressed topic

--- src/nodelet_test_node.cpp
+++ src/nodelet_test_node.cpp
@@ -2,6 +2,8 @@
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CompressedImage.h>
+
 
 namespace nodelet_test {
 
@@ -11,12 +13,17 @@ public:
     CameraForwardNodelet() {}
 
 private:
-    ros::Subscriber camera_sub_;
-    ros::Publisher camera_forward_pub_;
+    ros::Subscriber camera_sub_, compressed_sub_;
+    ros::Publisher camera_forward_pub_, compressed_forward_pub_;
 
     void callback(const sensor_msgs::ImageConstPtr &msg)
     {
         camera_forward_pub_.publish(msg);
+    }
+
+    void callback1(const sensor_msgs::CompressedImageConstPtr &msg)
+    {
+        compressed_forward_pub_.publish(msg);
     }
 
     virtual void onInit()
@@ -24,8 +31,10 @@ private:
         ros::NodeHandle &nh = getNodeHandle();
         ros::NodeHandle &private_nh = getPrivateNodeHandle();
 
-        camera_sub_ = nh.subscribe("/camera", 1, &CameraForwardNodelet::callback, this);
-        camera_forward_pub_ = nh.advertise<sensor_msgs::Image>("/camera_forward", 1);
+        camera_sub_ = nh.subscribe("/image_raw", 1, &CameraForwardNodelet::callback, this);
+        camera_forward_pub_ = nh.advertise<sensor_msgs::Image>("/image_raw_forward", 1);
+        compressed_sub_ = nh.subscribe("/image_raw/compressed", 1, &CameraForwardNodelet::callback1, this);
+        compressed_forward_pub_ = nh.advertise<sensor_msgs::CompressedImage>("/image_raw/compressed_forward", 1);
     }
 };
 
